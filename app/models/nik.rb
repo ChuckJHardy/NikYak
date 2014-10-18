385e0d3066a0691a337d1649919b5496f004a6e0
Edgars Jekabsons <jekabsons.edgars@gmail.com>
2014-10-18 04:13:55 +0200

    Fake data Helper methods to fetch last leaves

--- app/models/nik.rb
+++ app/models/nik.rb
@@ -2,4 +2,20 @@ class Nik < ActiveRecord::Base
   has_ltree_hierarchy
 
   belongs_to :user
+
+  def last_leaves_ids
+    Nik.
+      select("niks.id").
+      joins("LEFT JOIN niks AS niks2 ON niks.path @> niks2.path AND niks.path != niks2.path").
+      where("niks.path <@ ? AND niks.id != ?", path, id).
+      group("niks.id").
+      having("COUNT(niks2.id) = 0").
+      order("niks.path")
+  end
+
+  def last_leaves
+    Nik.
+      with(last_leaves_ids: last_leaves_ids).
+      joins("JOIN last_leaves_ids AS lli ON niks.id = lli.id")
+  end
 end
