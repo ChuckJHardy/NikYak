568e0e35b3118dbd6b20f57f2a07d2596a3d17fd
Edgars Jekabsons <jekabsons.edgars@gmail.com>
2014-10-18 03:57:39 +0200

    Enable ltree on Nik

--- app/models/nik.rb
+++ app/models/nik.rb
@@ -1,3 +1,5 @@
 class Nik < ActiveRecord::Base
+  has_ltree_hierarchy
+
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141018015235) do
+ActiveRecord::Schema.define(version: 20141018015441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20141018015235) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "parent_id"
+    t.ltree    "path"
   end
 
   add_index "niks", ["user_id"], name: "index_niks_on_user_id", using: :btree
