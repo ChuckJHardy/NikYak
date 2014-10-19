f4444918eb1b4fe0c7b3e6e6ce4e3c5b7fa2604b
Charles J Hardy <chuckjhardy@gmail.com>
2014-10-19 13:53:22 +0200

    Add Some Random Text

--- app/helpers/niks_helper.rb
+++ app/helpers/niks_helper.rb
@@ -1,2 +1,25 @@
 module NiksHelper
+  def random_change_link_left
+    [
+      "< Not for me",
+      "< Go Back",
+      "< This way Bro"
+    ].shuffle.first
+  end
+
+  def random_change_link_right
+    [
+      "Something has changed >",
+      "The Story has deviated >",
+      "What is happening over here? >"
+    ].shuffle.first
+  end
+
+  def random_change_link_vote
+    [
+      "Show some love",
+      "Vote",
+      "Do you think this is any good?"
+    ].shuffle.first
+  end
 end
