f2062ef67cc97de984dd2b47fa449ec7d0cef6e2
Charles J Hardy <chuckjhardy@gmail.com>
2014-10-19 15:55:10 +0200

    Make `Next` Text Random

--- app/helpers/niks_helper.rb
+++ app/helpers/niks_helper.rb
@@ -22,4 +22,14 @@ module NiksHelper
       "Do you think this is any good?"
     ].shuffle.first
   end
+
+  def random_change_link_what_now
+    [
+      "What happens next?",
+      "This is getting exciting! Now what?",
+      "Wrap it up!",
+      "I can hardly wait to see what happens next!",
+      "I'm on the edge of my seat! I can't imagine what's going to happen!"
+    ].shuffle.first
+  end
 end
